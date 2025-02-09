5c9da1cc76fef4574f8642482611ce1966ea51ea
Makwana Kanvi <163588203+kanvimakwana@users.noreply.github.com>
2025-02-10 02:57:23 +0530

    1.py

---
+++ Turtle_projects/1.py
@@ -0,0 +1,17 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("Green")
+timmy.forward(100)
+timmy.left(90)
+timmy.forward(100)
+timmy.left(90)
+timmy.forward(100)
+timmy.left(90)
+timmy.forward(100)
+timmy.left(90)
+
+
+screen  = Screen()
+screen.exitonclick()
